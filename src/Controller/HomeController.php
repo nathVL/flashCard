8176b2c697ecb847981dd56c8231a6911a18b684
Nathan Villette <nathan.villette@etudiant.univ-reims.fr>
2024-07-28 00:30:28 +0200

    Ajout du formulaire de création de dossier + css

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,9 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\File;
+use App\Form\AddFileType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 class HomeController extends AbstractController
 {
@@ -16,9 +22,39 @@ class HomeController extends AbstractController
             return $this->redirectToRoute('app_login');
         }
 
+
         return $this->render('home/index.html.twig', [
             'user' => $this->getUser(),
             'routeName' => 'app_home'
+        ]);
+    }
+
+    #[Route('/file', name: 'app_addFile')]
+    public function addFile(Request $request,SluggerInterface $slugger, EntityManagerInterface $em, ): Response
+    {
+        $file = new File();
+
+        $form = $this->createForm(AddFileType::class, $file);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $slug = strtolower($slugger->slug($file->getName()));
+            $file->setSlug($slug);
+            $file->setUser($this->getUser());
+            $file->setCreatedAt(new \DateTimeImmutable());
+            $file->setModifiedAt(new \DateTimeImmutable());
+
+            dd($file->getCreatedAt()->format('Y-m-d'));
+
+            $em->persist($file);
+            $em->flush();
+            return $this->redirectToRoute('app_home');
+        }
+
+        return $this->render('home/add.html.twig', [
+            'routeName' => 'app_addFile',
+            'form' => $form->createView()
         ]);
     }
 }
